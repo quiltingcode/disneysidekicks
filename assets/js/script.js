7769838d963de1bc333fee50eff30b58a46f8802
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-08-27 09:34:23 +0000

    Add more questions to the questions array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,4 +156,85 @@ let questions = [
         img: 'rajah.png'
         
     },
+    {
+        question : "What type of animal is Raya's pet, Tuk Tuk?",
+        answer1: "A horse",
+        answer2: "A dragon",
+        answer3: "An armadillo",
+        correct: "An armadillo",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "What is the name of Ariel's pet angelfish?",
+        answer1: "Sebastian",
+        answer2: "Scuttle",
+        answer3: "Flounder",
+        correct: "Flounder",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "Who becomes Marlin's unlikely friend and sidekick whilst in search of his son?",
+        answer1: "Dory",
+        answer2: "Sole",
+        answer3: "Haddock",
+        correct: "Dory",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "What is the name of Moana's brainless chicken?",
+        answer1: "Hei hei",
+        answer2: "Ho ho",
+        answer3: "Ha ha",
+        correct: "Hei hei",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "What magical sidekick does Elsa accidentally create as a child?",
+        answer1: "A reindeer",
+        answer2: "A snowman",
+        answer3: "A fire spirit",
+        correct: "A snowman",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "Which princess end up with seven trusty sidekicks whilst hiding in the forest?",
+        answer1: "Snow White",
+        answer2: "Cinderella",
+        answer3: "Aurora",
+        correct: "Snow White",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "Who takes care of Simba after he runs away from the Pridelands?",
+        answer1: "Shenzi and Banzai",
+        answer2: "Timon and Pumbaa",
+        answer3: "Zazu and Rafiki",
+        correct: "Timon and Pumbaa",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "What is the special talent of Aladdin's pet monkey?",
+        answer1: "Dancing",
+        answer2: "Flying",
+        answer3: "Stealing",
+        correct: "Stealing",
+        img: 'rajah.png'
+        
+    },
+    {
+        question : "What is the special talent of Aladdin's pet monkey?",
+        answer1: "Dancing",
+        answer2: "Flying",
+        answer3: "Stealing",
+        correct: "Stealing",
+        img: 'rajah.png'
+        
+    },
 ]
